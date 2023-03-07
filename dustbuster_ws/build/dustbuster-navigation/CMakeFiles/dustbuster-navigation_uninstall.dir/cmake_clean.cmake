b5849df0c99eba25ecedca07bd977d2e68f9863e
Onur Ulusoy <onurulusoys4@gmail.com>
2023-03-07 23:55:34 +0300

    Create workspace and navigation package

---
+++ dustbuster_ws/build/dustbuster-navigation/CMakeFiles/dustbuster-navigation_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dustbuster-navigation_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/dustbuster-navigation_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
