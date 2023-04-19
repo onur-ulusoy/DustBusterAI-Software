be7e7b0f5edbed50d069eff0df48c78f9f9d00a0
Onur Ulusoy <onurulusoys4@gmail.com>
2023-04-19 05:04:00 +0300

    Implement Extended Kalman Filter (EKF) for fusing IMU and wheel odometry in localization package

--- dustbuster_ws/src/dustbuster_launch/launch/cartographer.launch.py
+++ dustbuster_ws/src/dustbuster_launch/launch/cartographer.launch.py
@@ -57,7 +57,9 @@ def generate_launch_description():
             output='screen',
             parameters=[{'use_sim_time': use_sim_time}],
             arguments=['-configuration_directory', cartographer_config_dir,
-                       '-configuration_basename', configuration_basename]),
+                       '-configuration_basename', configuration_basename],
+            remappings=[('odom', '/odometry/filtered')]),
+            
 
         DeclareLaunchArgument(
             'resolution',
---
+++ dustbuster_ws/src/dustbuster_localization/launch/ekf_fusion.launch.py
@@ -0,0 +1,41 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_fusion',
+            parameters=[
+                {
+                    'imu0': '/imu',
+                    'odom0': '/odom',
+                    'odom_frame': 'odom',
+                    'base_link_frame': 'base_link',
+                    'world_frame': 'odom',
+                    'two_d_mode': True,
+                    'map_frame': '',
+                    'transform_time_offset': 0.0,
+                    'odom0_config': [False, False, False,
+                                     False, False, False,
+                                     True, True, True,
+                                     False, False, True,
+                                     False, False, False],
+                    'imu0_config': [False, False, False,
+                                    True, True, True,
+                                    False, False, False,
+                                    False, False, False,
+                                    True, True, True],
+                    'odom0_differential': False,
+                    'imu0_differential': False,
+                    'odom0_relative': False,
+                    'imu0_relative': False,
+                    'imu0_remove_gravitational_acceleration': True,
+                    'publish_tf': True,
+                    'publish_acceleration': False
+                }
+            ],
+            output='screen'
+        )
+    ])
