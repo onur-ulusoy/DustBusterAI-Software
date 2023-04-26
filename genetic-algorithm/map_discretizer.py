33ff0036ed9976d4bc40125c9d5a646acd754ba4
Onur Ulusoy <onurulusoys4@gmail.com>
2023-04-26 17:25:08 +0300

    Enhence map_discretizer to set origin and resolution correctly

--- genetic-algorithm/map_discretizer.py
+++ genetic-algorithm/map_discretizer.py
@@ -11,31 +11,39 @@ def discretize_walkable_area(image, sample_rate):
     sampled_area = walkable_area[::sample_rate, ::sample_rate]
     return np.where(sampled_area == 1)
 
-def plot_walkable_area(coords, sample_rate):
+def plot_walkable_area(coords, image_shape, sample_rate, resolution, origin):
     y, x = coords
-    x_scaled = x * sample_rate
-    y_scaled = y * sample_rate
-    plt.scatter(x_scaled, y_scaled, c='red', marker='.')
-    plt.gca().invert_yaxis()
-    plt.gca().set_aspect('equal', adjustable='box')
+    x_scaled = x * sample_rate * resolution + origin[0]
+    y_scaled = origin[1] + image_shape[0] * resolution - y * sample_rate * resolution
+
+    fig, ax = plt.subplots()
+    ax.scatter(x_scaled, y_scaled, c='red', marker='.')
+    ax.set_xlim(origin[0], origin[0] + image_shape[1] * resolution)
+    ax.set_ylim(origin[1], origin[1] + image_shape[0] * resolution)
+    ax.set_aspect('equal', adjustable='box')
+
     plt.show()
 
-def print_points(coords, sample_rate):
+def print_points(coords, image_shape, sample_rate, resolution, origin):
     y, x = coords
-    points = np.vstack((x * sample_rate, y * sample_rate)).T
+    points = np.vstack((x * sample_rate * resolution + origin[0], origin[1] + image_shape[0] * resolution - y * sample_rate * resolution)).T
     for point in points:
         print(f"({point[0]}, {point[1]})")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print(f"Usage: {sys.argv[0]} <PGM_FILE> <SAMPLE_RATE>")
+    if len(sys.argv) != 5:
+        print(f"Usage: {sys.argv[0]} <PGM_FILE> <SAMPLE_RATE> <ORIGIN_X> <ORIGIN_Y>")
         sys.exit(1)
 
     pgm_file = sys.argv[1]
     sample_rate = int(sys.argv[2])
+    origin_x = float(sys.argv[3])
+    origin_y = float(sys.argv[4])
 
     image = load_pgm_image(pgm_file)
     coords = discretize_walkable_area(image, sample_rate)
+    resolution = 0.05
+    origin = (origin_x, origin_y)
 
-    plot_walkable_area(coords, sample_rate)
-    print_points(coords, sample_rate)
+    plot_walkable_area(coords, image.shape, sample_rate, resolution, origin)
+    print_points(coords, image.shape, sample_rate, resolution, origin)
