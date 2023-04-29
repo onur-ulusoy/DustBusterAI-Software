6a88fe5001f274e7d43d406b64146e75d0f4eb2f
Onur Ulusoy <onurulusoys4@gmail.com>
2023-04-29 11:10:10 +0300

    Add waypoint markers and debug lines, fix waypoint parsing issue in WaypointsNavigation node

--- dustbuster_ws/src/dustbuster_navigation/dustbuster_navigation/WaypointsNavigation.py
+++ dustbuster_ws/src/dustbuster_navigation/dustbuster_navigation/WaypointsNavigation.py
@@ -4,7 +4,7 @@ from std_msgs.msg import String
 from nav2_msgs.action import FollowWaypoints
 from rclpy.action import ActionClient
 from geometry_msgs.msg import PoseStamped
-
+from visualization_msgs.msg import Marker, MarkerArray
 
 class WaypointsNavigation(Node):
 
@@ -18,19 +18,27 @@ class WaypointsNavigation(Node):
         self.subscription  # prevent unused variable warning
         self.action_client = ActionClient(self, FollowWaypoints, 'follow_waypoints')
         self.waypoints = []
+        self.marker_array_pub = self.create_publisher(MarkerArray, 'waypoints_markers', 10)
 
     def waypoints_callback(self, msg):
-        data = msg.data.replace(",", ";").split(";")
-        x, y = float(data[0].strip()), float(data[1].strip())
-        waypoint = PoseStamped()
-        waypoint.header.frame_id = 'map'
-        waypoint.pose.position.x = x
-        waypoint.pose.position.y = y
-        waypoint.pose.orientation.w = 1.0
-        self.waypoints.append(waypoint)
+        data = msg.data.strip().split(';')
+        for point in data:
+            coords = point.split(',')
+            x, y = float(coords[0].strip()), float(coords[1].strip())
+            waypoint = PoseStamped()
+            waypoint.header.frame_id = 'map'
+            waypoint.pose.position.x = x
+            waypoint.pose.position.y = y
+            waypoint.pose.orientation.w = 1.0
+            self.waypoints.append(waypoint)
+            
+            # Debug prints
+            self.get_logger().info(f"Received waypoint: x={x}, y={y}")
+            
+        self.get_logger().info(f"Number of waypoints: {len(self.waypoints)}")
+        self.publish_markers()
+        self.send_navigation_goal()  # Add this line
 
-        # Send navigation goal after every new waypoint received
-        self.send_navigation_goal()
 
     def send_navigation_goal(self):
         if not self.action_client.wait_for_server(timeout_sec=10.0):
@@ -42,7 +50,27 @@ class WaypointsNavigation(Node):
         self.action_client.send_goal_async(goal_msg)
 
         self.get_logger().info('Sent navigation goal with waypoints.')
-
+    
+    def publish_markers(self):
+        marker_array = MarkerArray()
+        for idx, waypoint in enumerate(self.waypoints):
+            marker = Marker()
+            marker.header.frame_id = 'map'
+            marker.type = Marker.SPHERE
+            marker.action = Marker.ADD
+            marker.pose.position.x = waypoint.pose.position.x
+            marker.pose.position.y = waypoint.pose.position.y
+            marker.pose.orientation.w = 1.0
+            marker.id = idx
+            marker.scale.x = 0.2
+            marker.scale.y = 0.2
+            marker.scale.z = 0.2
+            marker.color.a = 1.0
+            marker.color.r = 0.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            marker_array.markers.append(marker)
+        self.marker_array_pub.publish(marker_array)
 
 
 def main(args=None):
